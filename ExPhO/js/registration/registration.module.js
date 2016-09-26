b45f0d8c05527deadaf0d73e2c974a3b554e25c4
Andrii Kobzar <Andrii Kobzar>
2016-09-26 15:30:31 +0300

    adding routes of ui router, reistration module

--- ExPhO/js/registration/registration.module.js
+++ ExPhO/js/registration/registration.module.js
@@ -1,5 +1,18 @@
 ﻿(function () {
     angular.module("Registration", ["ui.router"]).
-    config().
+    config(function($stateProvider, $urlRouteProvider){
+        $urlRouterProvider.otherwise('/role');
+        $stateProvider.
+            state('role', {
+                url:'/role',
+                templateUrl:'/html/registration/role.html',
+                controller:'roleController'
+            }).
+            state('info', {
+                url:'/info',
+                templateUrl:'/html/registration/info.html',
+                controller: 'infoController'
+            })
+    }).
     run();
 })();
--- ExPhO/js/registration/roleStep.controller.js
+++ ExPhO/js/registration/roleStep.controller.js
@@ -1,5 +1,19 @@
 ﻿(function (){
     angular.module("Registration").controller("RoleController", function () {
         var self = this;
+        self.roles = {
+            JURY: 'Jury',
+            TEACHER: 'Teacher',
+            LEARNER:'Learner'
+        }
+        self.userRole = null;
+        self.name = null;
+        self.surname = null;
+
+        self.nextStep = nextStep;
+
+        function nextStep(){
+            
+        }
     });
 })();
